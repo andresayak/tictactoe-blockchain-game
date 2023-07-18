4090c56969cc0fcc664d791cbde01b2151ab99bd
Andrii Saiak <andresayak@gmail.com>
2023-07-18 12:08:46 +0300

    updated README.md

--- blockchain_app/README.md
+++ blockchain_app/README.md
@@ -1,6 +1,5 @@
-# Sample Hardhat Project
+# TicTacToe contracts
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
 
 Try running some of the following tasks:
 
@@ -9,5 +8,25 @@ npx hardhat help
 npx hardhat test
 REPORT_GAS=true npx hardhat test
 npx hardhat node
-npx hardhat run scripts/deploy.ts
+npx hardhat run scripts/deploy_factory.ts
+```
+
+### Deploy contracts
+
+#### Testnet
+
+```shell
+npx hardhat run scripts/deploy_factory.ts --network bsc_testnet
+```
+
+#### Mainnet
+
+```shell
+npx hardhat run scripts/deploy_factory.ts --network bsc_mainnet
+```
+
+#### Verify contracts
+
+```shell
+npx hardhat verify --network <bsc_testnet|bsc_mainnet> <factoryAddress>
 ```
--- blockchain_app/hardhat.config.ts
+++ blockchain_app/hardhat.config.ts
@@ -1,29 +1,61 @@
-import { HardhatUserConfig } from "hardhat/config";
+import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-contract-sizer";
 import "hardhat-gas-reporter";
+import {task} from "hardhat/config";
 
 const {config: dotEnvConfig} = require("dotenv");
 
 dotEnvConfig({path: __dirname + '/.env'});
 
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners();
+    for (const account of accounts) {
+        console.log(account.address);
+    }
+});
+
+task("accountsWithBalances", "Prints the list of accounts with balances", async (taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners();
+    for (const account of accounts) {
+        let balance = (await account.getBalance()).toString();
+        console.log(account.address, balance);
+    }
+});
+
 const config: HardhatUserConfig = {
-  solidity: {
-    version: "0.8.19",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 100,
-      },
+    solidity: {
+        version: "0.8.19",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+        },
     },
-  },
-  gasReporter: {
-    enabled: true,
-    currency: 'USD',
-    gasPrice: 5,
-    token: 'BNB',
-    coinmarketcap: process.env.COINMARKETCAP_KEY,
-  }
+    networks: {
+        bsc_mainnet: {
+            chainId: 56,
+            url: `https://bsc-dataseed.binance.org/`,
+            accounts: [process.env.TBSC_PRIVATE_KEY || ''],
+            gas: 5000000,
+            gasPrice: 5000000000,
+        },
+        bsc_testnet: {
+            chainId: 97,
+            url: `https://data-seed-prebsc-1-s2.binance.org:8545`,
+            accounts: [process.env.BSC_PRIVATE_KEY || ''],
+            gas: 500000,
+            gasPrice: 15000000000,
+        }
+    },
+    gasReporter: {
+        enabled: process.env.REPORT_GAS ? true : false,
+        currency: 'USD',
+        gasPrice: 5,
+        token: 'BNB',
+        coinmarketcap: process.env.COINMARKETCAP_KEY,
+    }
 };
 
 export default config;
